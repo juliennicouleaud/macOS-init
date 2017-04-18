9cafc3a5ec449a2ca5d94d4f98a8c8740ba003f5
Boris SCHAPIRA <borisschapira@gmail.com>
2017-04-18 15:08:19 +0200

    Update cask with a buffer array for better update management

--- update.sh
+++ update.sh
@@ -2,15 +2,15 @@
 
 set -x
 # Update brew and upgrade all packages
-brew update
-brew outdated
-brew upgrade
-brew cleanup
-brew doctor
-brew prune
+# brew update
+# brew outdated
+# brew upgrade
+# brew cleanup
+# brew doctor
+# brew prune
 
 # Clean Cask cache
-brew cask cleanup
+# brew cask cleanup
 
 set +x
 
@@ -20,17 +20,31 @@ reset=`tput sgr0`
 
 # List all casks and for each of them,
 # test if a newer version is available.
+indexUpdateAvailable=0
+declare -a updateAvailable=()
 casks=( $(brew cask list) )
+
+echo "Scanning the casks…"
 for cask in ${casks[@]}
 do
     version=$(brew cask info $cask | sed -n "s/$cask:\ \(.*\)/\1/p")
     installed=$(find "/usr/local/Caskroom/$cask" -type d -maxdepth 1 -maxdepth 1 -name "$version")
-
     if [[ -z $installed ]]; then
-        echo "${red}${cask}${reset} requires ${red}update${reset}."
-        (set -x; brew cask uninstall $cask --force;)
-        (set -x; brew cask install $cask --force;)
+        updateAvailable[$indexUpdateAvailable]="${cask}"
+        indexUpdateAvailable=$((indexUpdateAvailable+1))
     else
         echo "${red}${cask}${reset} is ${green}up-to-date${reset}."
+    fi
+done
+for updateAvailable in ${updateAvailable[@]}
+do
+    read -p "${red}${updateAvailable}${reset} requires ${red}update${reset}. Do youn want to update? (${green}y${reset}/${red}n${reset}) Default: ${red}no${reset} " -n 1 -r
+    echo    # (optional) move to a new line
+    if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+        echo "Ok! Doing nothing."
+    else
+        echo "Ok! Let's update ${green}${updateAvailable}${green}!"
+        (set -x; brew cask uninstall $updateAvailable --force;)
+        (set -x; brew cask install $updateAvailable --force;)
     fi
 done
