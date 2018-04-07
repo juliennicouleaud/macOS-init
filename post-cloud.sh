4d3c579ece87441467b7f5810ecf5e3f139b84fd
Julien Nicouleaud <juliennicouleaud@yahoo.fr>
2018-04-07 15:10:31 +0200

    Changed mackup conf to use Google Drive, removed installation of oh-my-zsh
    Need to add installation procedure for Prezto

--- post-cloud.sh
+++ post-cloud.sh
@@ -4,8 +4,8 @@
 # /!\ Ce script d'installation est conçu pour mon usage. Ne le lancez pas sans vérifier chaque commande ! /!\
 
 echo "Restauration des préférences"
-# Sélection du service de cloud (à décommenter si vous n'utilisez pas Dropbox, c'est le service par défaut) : https://github.com/lra/mackup/blob/master/doc/README.md
-# echo -e "[storage]\nengine = google_drive" >> ~/.mackup.cfg
+# Sélection du service de cloud (à commenter si vous utilisez Dropbox, c'est le service par défaut) : https://github.com/lra/mackup/blob/master/doc/README.md
+echo -e "[storage]\nengine = google_drive" >> ~/.mackup.cfg
 
 # Récupération de la sauvegarde sans demander à chaque fois l'autorisation
 mackup restore -n
@@ -14,9 +14,13 @@ mackup restore -n
 # defaults write com.apple.screencapture location -string "$HOME/Dropbox/Screenshots/"
 # Attention, nécessite un redémarrage ou un `killall SystemUIServer` pour être pris en compte
 
-echo "Installation de oh-my-zsh"
+# echo "Installation de oh-my-zsh"
 # Installation de oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+###
+### Ajouter ici l'install de Prezto - https://github.com/sorin-ionescu/prezto
+###
 
 echo ""
 echo "ET VOILÀ !"
